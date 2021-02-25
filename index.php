312b8df3c82bdef6bd371b99c87e67a4ffc4940a
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-25 22:17:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <p>
 <body>
 <center>
- <body bgcolor= "peach"> 
+ <body bgcolor= "beige"> 
        <p> <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/153486284_486288725725940_1727376092548401377_n.jpg?_nc_cat=101&ccb=3&_nc_sid=ae9488&_nc_ohc=dZx_I3HBu0gAX-G82dT&_nc_ht=scontent.fceb2-1.fna&oh=db627a6876c14e7c14b42ad292c0dde0&oe=605D1014", width = 210 , length = 210></img><br>
  <font color= "black"> CLOCK </font> </center>
  </body>

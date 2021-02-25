f1004f5bf1494a9a5de5e09190ae66ff4f8bb53d
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-25 23:02:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <center>
  <body>
 <body bgcolor= "fuschia">
- <iframe src="https://open.spotify.com/embed/album/1xWYSg7J7pxTZ113CJVy1P" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
  <br> <br>
        <p> <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/153486284_486288725725940_1727376092548401377_n.jpg?_nc_cat=101&ccb=3&_nc_sid=ae9488&_nc_ohc=dZx_I3HBu0gAX-G82dT&_nc_ht=scontent.fceb2-1.fna&oh=db627a6876c14e7c14b42ad292c0dde0&oe=605D1014", width = 210 , length = 210></img><br>
  <p>  <font color= "yellow"> <font size="4"> <b> <marquee>  CLOCK  </marquee> </b> </font> </center>

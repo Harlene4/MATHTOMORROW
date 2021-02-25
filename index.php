017500bbe7fa761d6d2c5b3a577acf0ba8a342a2
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-25 22:25:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
 <p>
 <body>
 <center>
- <body bgcolor= "beige"> 
+ <body backgorund="https://www.clipartkey.com/mpngs/m/4-43161_vector-download-flowers-clipart-transparent-transparent-background-lily.png">
+  <br><br>
+  <br><br>
        <p> <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/153486284_486288725725940_1727376092548401377_n.jpg?_nc_cat=101&ccb=3&_nc_sid=ae9488&_nc_ohc=dZx_I3HBu0gAX-G82dT&_nc_ht=scontent.fceb2-1.fna&oh=db627a6876c14e7c14b42ad292c0dde0&oe=605D1014", width = 210 , length = 210></img><br>
- <font color= "pink"><b> <marquee> <f4> CLOCK </f4> </marquee> </b> </font> </center>
+ <p>  <font color= "pink"><b> <marquee> <f4> CLOCK </f4> </marquee> </b> </font> </center>
  </body>
 </html>

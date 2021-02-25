769200b936bcf3fb051bad45735c93ae44f8f9e4
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-25 22:13:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <p>
 <p>
 <body>
-<center>
-<body backgound= "https://www.schemecolor.com/wallpaper?i=34078&desktop", width = 1200 , length = 800> </img>
-    >
- <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/153486284_486288725725940_1727376092548401377_n.jpg?_nc_cat=101&ccb=3&_nc_sid=ae9488&_nc_ohc=dZx_I3HBu0gAX-G82dT&_nc_ht=scontent.fceb2-1.fna&oh=db627a6876c14e7c14b42ad292c0dde0&oe=605D1014", width = 210 , length = 210></img><br>
+<p align= center> <body backgound= "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.marthastewart.com%2Fshop%2Fivy-bronx-charette-digital-33-x-21-solid-wallpaper-roll-ivy-bronx-color-light-blue-p3b519be606fb1d23a9d275ca72d1d256.html&psig=AOvVaw3c68UZzRVi-38ZX4-VyR7T&ust=1614348725827000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIi9meubhe8CFQAAAAAdAAAAABAD", , width = 1200 , length = 800> </img>
+     <p>
+      <p>
+       <p> <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/153486284_486288725725940_1727376092548401377_n.jpg?_nc_cat=101&ccb=3&_nc_sid=ae9488&_nc_ohc=dZx_I3HBu0gAX-G82dT&_nc_ht=scontent.fceb2-1.fna&oh=db627a6876c14e7c14b42ad292c0dde0&oe=605D1014", width = 210 , length = 210></img><br>
  <font color= "black"> CLOCK </font> </center>
  </body>
 </html>

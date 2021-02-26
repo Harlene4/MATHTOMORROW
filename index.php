8dcb703b42409339fda0245a4ebec0f59814d9fd
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-26 15:40:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <html>
-<head> <P ALIGN= CENTER> <FONT SIZE="15"> MATH FINAL PT </FONT> </CENTER> </head>
+<head> <P ALIGN= CENTER> <FONT SIZE="17"> MATH FINAL PT </FONT> </CENTER> </head>
  <title> MATH TOMORROW </title>
 <center>
  <body>
 <body bgcolor= "fuschia">
- <br> <font color= "black"> <font size= "6"> <b> An Image of HOPE </b> </font>
- 
+ <br> <style>
+h1 {
+  color: white;
+  text-shadow: 1px 1px 2px black, 0 0 25px purple, 0 0 5px lightgreen;
+}
+</style>
+<center> <b><font color="white", size= "7">The image of HOPE </font> </b></center>
        <p> <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t1.15752-9/153486284_486288725725940_1727376092548401377_n.jpg?_nc_cat=101&ccb=3&_nc_sid=ae9488&_nc_ohc=dZx_I3HBu0gAX-G82dT&_nc_ht=scontent.fceb2-1.fna&oh=db627a6876c14e7c14b42ad292c0dde0&oe=605D1014", width = 210 , length = 210></img>
 <BR> <p>  <font color= "purple"> <font size="5"> <b> <marquee>  CLOCK  </marquee> </b> </font> </center>
  <b> <b>
